3bbeda86a1eb9e39c01820f768672dfb2a1a003f
Akshaya Nandakumar <akshayanandakumar22@gmail.com>
2026-04-08 21:56:49 +0530

    Add expressions.java with basic arithmetic operations

---
+++ src/basics/expressions.java
@@ -0,0 +1,39 @@
+package project.one;
+
+public class expressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		//addition
+		int x = 10;
+		x = x + 1;
+		System.out.println(x);
+		
+		//subtraction 
+		int y = 10;
+		y = y - 1;
+		System.out.println(y);
+		
+		//multiplication 
+		int z = 10;
+		z = z * 2;
+		System.out.println(z);
+		
+		//division -- output is usually a double value, but here since u is predefined as an integer, its value remains an integer
+		int u = 10;
+		u = u / 4;
+		System.out.println(u);
+		
+		//modulus
+		int k = 10;
+		k = k % 3;
+		System.out.println(k);
+		
+				
+		
+	}
+
+}
